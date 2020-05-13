041f830ba169ecd0707f0a99f7d06674126e554a
reddevilcero <reddevil_cero@hotmail.com>
2020-05-13 16:00:51 +0100

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
+
+var random = Math.random();
+var mod = 10 % 6;
+var arr = [1, 2, 3, 4, 5, 20];
+var max = Math.max(...arr);
